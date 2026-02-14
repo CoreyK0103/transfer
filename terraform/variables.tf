bf382292f87a79824f96b1a222e2584a32f2e906
CoreyK0103 <coreyjknight@outlook.com>
2026-02-14 11:36:43 +0000

    feat: malware protection on landing bucket

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,11 @@ variable "lambda_runtime" {
   description = "The runtime environment for the Lambda function."
   type        = string
 }
+
+variable "account_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
