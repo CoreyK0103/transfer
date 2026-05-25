c809008040b9fdf82da75839b51298891825bfbd
Corey Knight <142681177+CoreyK0103@users.noreply.github.com>
2026-05-25 13:19:18 +0100

    ci: oidc (#6)
    * ci: oidc
    
    * chore: tsconfig updates

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "file_info" {
-  name         = "file-info-table"
+  name         = "${local.resource_prefix}-file-info-table"
   hash_key     = "fileId"
   billing_mode = "PAY_PER_REQUEST"
 
