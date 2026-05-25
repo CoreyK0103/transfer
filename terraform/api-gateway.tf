081f1222c45534e08c098ff647ce398af250dad4
CoreyK0103 <coreyjknight@outlook.com>
2026-05-25 13:26:36 +0100

    feat: jwt auth

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -7,6 +7,20 @@ resource "aws_apigatewayv2_api" "presigned_url_api" {
   }
 }
 
+resource "aws_apigatewayv2_authorizer" "cognito" {
+  api_id = aws_apigatewayv2_api.api.id
+  name   = "${local.resource_prefix}-cognito-authorizer"
+
+  authorizer_type = "JWT"
+
+  identity_sources = ["$request.header.Authorization"]
+
+  jwt_configuration {
+    audience = [aws_cognito_user_pool_client.client.id]
+    issuer   = "https://cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.pool.id}"
+  }
+}
+
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id                 = aws_apigatewayv2_api.presigned_url_api.id
   integration_type       = "AWS_PROXY"
@@ -15,9 +29,11 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
 }
 
 resource "aws_apigatewayv2_route" "post_route" {
-  api_id    = aws_apigatewayv2_api.presigned_url_api.id
-  route_key = "POST /"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  api_id             = aws_apigatewayv2_api.presigned_url_api.id
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.cognito.id
+  route_key          = "POST /"
+  target             = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
 resource "aws_apigatewayv2_stage" "default_stage" {
