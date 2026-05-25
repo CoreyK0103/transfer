5efe967cb642a55d5095693e3966b87f77274158
CoreyK0103 <coreyjknight@outlook.com>
2026-05-25 13:28:04 +0100

    chore: outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "api_endpoint" {
   value = aws_apigatewayv2_api.presigned_url_api.api_endpoint
 }
+
+output "user_pool_id" {
+  value = aws_cognito_user_pool.pool.id
+}
+
+output "client_id" {
+  value = aws_cognito_user_pool_client.client.id
+}
